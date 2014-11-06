829e9455f558c4d25fcf2ebd98fbdd7cecf528fd
zhangzhishan <zhangzhishanlo@gmail.com>
2014-11-06 15:41:22 +0800

    改为命令行调用模式增加两个参数，依次为货物数量和背包容量

--- test.py
+++ test.py
@@ -1,19 +1,28 @@
+
 import json
 import random
 import time
-num = 100
-f = open('weight.txt', 'w+')
-weight = []
-random.seed(time.time())
-for i in range(num):
-	weight.append(int(random.triangular(1, 5 * num)))
-json.dump(weight, f)
-f.close()
-f = open('value.txt', 'w+')
-value = []
-random.seed(time.time())
-for i in range(num):
-	value.append(int(random.triangular(1, 5 * num)))
-json.dump(value, f)
-f.close()
+import sys
+def test(num, cap):
+    num = int(num)
+    cap = int(cap)
+    f = open('cap.txt', 'w+')
+    json.dump(cap, f)
+    f.close()
+    f = open('weight.txt', 'w+')
+    weight = []
+    random.seed(time.time())
+    for i in range(num):
+        weight.append(random.triangular(0, 1))
+    json.dump(weight, f)
+    f.close()
+    f = open('value.txt', 'w+')
+    value = []
+    random.seed(time.time())
+    for i in range(num):
+        value.append(int(random.triangular(1, 5 * num)))
+    json.dump(value, f)
+    f.close()
 
+if __name__ == '__main__':
+    test(sys.argv[1], sys.argv[2])
