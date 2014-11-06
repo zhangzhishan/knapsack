c2a03ffb7e86cbea922fcda5095e2226b432b6cd
zhangzhishan <zhangzhishanlo@gmail.com>
2014-11-06 15:47:40 +0800

    从文件中读取cap值

--- blind_greedy.py
+++ blind_greedy.py
@@ -22,16 +22,24 @@ def greedydp(cap, weight=[], value=[], I=[]):
 def calcu_time():
     start = time.time()
     #print start
-    num = 100
-    cap = 10000
+    #num = 100
+    #cap = 10000
     f = open('weight.txt', 'r')
     weight = json.load(f)
     f.close()
     f = open('value.txt', 'r')
     value = json.load(f)
     f.close()
-    I = [i for i in range(num)]
-    print greedydp(cap, weight, value, I)
+    f = open('cap.txt', 'r')
+    cap = json.load(f)
+    f.close()
+    I = [i for i in range(len(weight))]
+    S, U = greedydp(cap, weight, value, I)
+    print S, U
+    weight_sum = 0
+    for i in S:
+        weight_sum += weight[i]
+    print 'weight_sum: ', weight_sum
     #print time.time()
     timeused = time.time() - start
     print '用时：', timeused
